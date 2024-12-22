e5e1ca88bf4d82c0ba76e9b8db1cf2c0ed4bee7f
galibcse19 <mrgalib40@gmail.com>
2024-12-22 13:35:37 +0600

    design register and log In page with responsive

--- Service-Review-System-Client/src/Page/Login/Login.jsx
+++ Service-Review-System-Client/src/Page/Login/Login.jsx
@@ -1,9 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [error,setError] = useState('');
+    const [message, setMessage] = useState('');
+    const navigate = useNavigate();
+    const location =useLocation();
+    const from =location.state?.from?.pathname || "/";
+
+    const onSubmit  = data =>{
+        // signIn(data.email,data.password)
+        // .then(result =>{
+        //     const user = result.user;
+        //     toast.success('LogIn successfully done.',{position: "top-center"});
+        //     navigate(from, {replace:true});
+        // })
+        // .catch((error) => {
+        //     setError(error.message); 
+        //   });
+    } 
+
+    const handelGoogleSignIn =()=>{
+        // signInWithGoogle()
+        // .then((result)=>{
+        //     const user=result.user;
+        //     console.log(user);
+        //     toast.success('LogIn successfully done.',{position: "top-center"});
+        //     navigate(from, {replace:true});
+        // })
+        // .catch((error)=>{
+        //     setError(error);
+        // })
+    }
     return (
         <div>
-            Login
+            <div className="relative bg-cover bg-center py-10">
+        <div className="absolute inset-0 bg-gradient-to-r from-green-600 to-green-600 bg-opacity-50" />
+
+            {/* Upper Section with Glassmorphism */}
+            <div className="flex flex-col items-center justify-center px-4">
+                {/* Welcome Section */}
+                <div className="backdrop-blur-md bg-white bg-opacity-30 border border-white border-opacity-30 rounded-lg p-6 m-4 w-full max-w-md shadow-lg text-center">
+                <h1 className="text-xl md:text-3xl lg:text-3xl font-bold text-white">WELCOME TO SERVICE REVIEW</h1>
+                </div>
+
+                {/* Login Form Section */}
+                <div className="backdrop-blur-md bg-white bg-opacity-30 border border-white border-opacity-30 rounded-lg lg:p-8 md:p-6 p-4 m-4 w-full max-w-md shadow-lg">
+                <h2 className="text-xl md:text-2xl font-bold mb-6 text-center text-white">LOG IN</h2>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="mb-4">
+                    <label className="block text-sm font-medium text-white" htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        {...register("email",{ required: true })}
+                        className="mt-1 lg:p-4 md:p-4 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-500 bg-transparent text-white"
+                        required
+                    />
+                    </div>
+                    <div className="mb-4">
+                    <label className="block text-sm font-medium text-white" htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        {...register("password",{ required: true })}
+                        className="mt-1 lg:p-4 md:p-4 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-500 bg-transparent text-white"
+                        required
+                    />
+                    {message && <p className="text-center text-red-600 text-sm mt-4">{message}</p>}
+                    </div>
+                    <button
+                    type="submit"
+                    className="w-full font-bold lg:p-4 md:p-4 p-2 bg-green-600 text-white rounded-md hover:bg-red-500 transition duration-200"
+                    >
+                    LOG IN
+                    </button>
+                </form>
+                {
+                    error && <p className='text-red-600'>{error}</p>
+                }
+                <p className="text-white my-2">
+                    Don't have an Account? <a className="font-bold" href="/register">Register</a>
+                </p>
+                <div className="divider divider-success text-white">OR</div>
+                <p><button onClick={handelGoogleSignIn} className="w-full font-bold lg:p-4 md:p-4 p-2 bg-green-600 text-white rounded-md hover:bg-red-500">Log in with Google</button></p>
+                </div>
+            </div>
+        </div>
         </div>
     );
 };
--- Service-Review-System-Client/src/Page/Register/Register.jsx
+++ Service-Review-System-Client/src/Page/Register/Register.jsx
@@ -1,9 +1,142 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Register = () => {
+    const [error,setError] = useState('');
+    const navigate = useNavigate();
+    const location =useLocation();
+    const from =location.state?.from?.pathname || "/";
+
+    const handleRegister = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const photo = form.url.value;
+        const password = form.password.value;
+
+        // Password validation check
+        const isValidPassword = validatePassword(password);
+        if (!isValidPassword) {
+        setError(
+            'Password must be at least 6 characters long and include both uppercase and lowercase letters.'
+        );
+        return;
+        }
+        setError(''); 
+
+        // createUser(email,password)
+        //  .then(result =>{
+        //     const user = result.user;
+        //     setUser(user);
+        //     console.log(user);
+        //     updateUserProfile({displayName:name,photoURL:photo})
+        //     .then(()=>{
+        //         setUser((prevUser) => ({
+        //             ...prevUser,
+        //             displayName: name,
+        //             photoURL: photo
+        //           }));
+        //     })
+        //     .catch((error)=>{
+        //         setError(error.message);
+        //     })
+        //     toast.success('Register successfully done.',{position: "top-center"});
+        //     navigate(from, {replace:true});
+        //  })
+        //  .catch((error) => {
+        //     setError(error.message); 
+        //   });
+     
+   };
+    const handelGoogleSignIn =()=>{
+        // signInWithGoogle()
+        // .then((result)=>{
+        //     const user=result.user;
+        //     console.log(user);
+        //     toast.success('Register successfully done.',{position: "top-center"});
+        //     navigate(from, {replace:true});
+        // })
+        // .catch((error)=>{
+        //     setError(error);
+        // })
+    }
+    const validatePassword = (password) => {
+        const hasUppercase = /[A-Z]/.test(password);
+        const hasLowercase = /[a-z]/.test(password);
+        const isValidLength = password.length >= 6;
+    
+        return hasUppercase && hasLowercase && isValidLength;
+      };
+
     return (
         <div>
-            Register
+            <div className="relative bg-cover bg-center py-10">
+                <div className="absolute inset-0 bg-gradient-to-r from-green-600 to-green-600 bg-opacity-50" />
+
+                {/* Upper Section with Glassmorphism */}
+                <div className="flex flex-col items-center justify-center px-4">
+                    {/* Welcome Section */}
+                    <div className="backdrop-blur-md bg-white bg-opacity-30 border border-white border-opacity-30 rounded-lg p-6 m-4 w-full max-w-md shadow-lg text-center">
+                    <h1 className="text-xl md:text-3xl lg:text-3xl font-bold text-white">WELCOME TO SERVICE REVIEW</h1>
+                    </div>
+
+                    {/* Login Form Section */}
+                    <div className="backdrop-blur-md bg-white bg-opacity-30 border border-white border-opacity-30 rounded-lg p-8 m-4 w-full max-w-md shadow-lg">
+                    <h2 className="text-xl md:text-2xl font-bold mb-6 text-center text-white">REGISTER NOW</h2>
+                    <form  onSubmit={handleRegister}>
+                        <div className="mb-4">
+                        <label className="block text-sm font-medium text-white" htmlFor="email">Name</label>
+                        <input
+                            type="text"
+                            name="name"
+                            placeholder='Enter Your Name'
+                            className="mt-1 p-4 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-500 bg-transparent text-white"
+                        />
+                        </div>
+                        <div className="mb-4">
+                        <label className="block text-sm font-medium text-white" htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder='Enter Your Email'
+                            className="mt-1 p-4 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-500 bg-transparent text-white"
+                        />
+                        </div>
+                        <div className="mb-4">
+                        <label className="block text-sm font-medium text-white" htmlFor="email">PhotoURL</label>
+                        <input
+                            type="url"
+                            name="url"
+                            placeholder='Enter Your PhotoURL'
+                            className="mt-1 p-4 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-500 bg-transparent text-white"
+                        />
+                        </div>
+                        <div className="mb-4">
+                        <label className="block text-sm font-medium text-white" htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder='Enter Password'
+                            className="mt-1 p-4 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-500 bg-transparent text-white"
+                        />
+                        </div>
+                        <button
+                        type="submit"
+                        className="w-full font-bold p-4 bg-green-600 text-white rounded-md hover:bg-red-500 transition duration-200"
+                        >
+                        REGISTER
+                        </button>
+                    </form>
+                    {
+                    error && <p className='text-red-600'>{error}</p>
+                    }
+                    <p className='text-white my-2'>Already have an Account? <a className='font-bold my-2' href="/logIn">Log In</a></p>
+                    <div className="divider divider-success text-white">OR</div>
+                    <p><button onClick={handelGoogleSignIn} className="w-full font-bold lg:p-4 md:p-4 p-2 bg-green-600 text-white rounded-md hover:bg-red-500">Log in with Google</button></p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
